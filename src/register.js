b3d25e92cfbaf0a27c5435138b03e02e20bace9c
hugojoensson <hugojoensson@gmail.com>
2024-04-25 15:58:11 +0200

    fungerar

--- src/register.js
+++ src/register.js
@@ -1,3 +1,11 @@
+import path from "path";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default function movieReg(db) {
   return async function (req, res) {
     let title = req.query.title;
@@ -6,7 +14,14 @@ export default function movieReg(db) {
     let movie_length = req.query.movie_length;
     let rating = req.query.rating;
 
-    let question = 'INSERT INTO movie (title , release_year, watch_date, movie_length, rating) VALUES ("'+title+'","'+release_year+'","'+watch_date+'","'+movie_length+'","'+rating+'")';
-    await db.execute(question);
+      let question = 'INSERT INTO movie (title , release_year, watch_date, movie_length, rating) VALUES ("'+title+'","'+release_year+'","'+watch_date+'","'+movie_length+'","'+rating+'")';
+      await db.execute(question);
+/*
+      const [rows] = await db.execute('SELECT * FROM movie ORDER BY id DESC LIMIT 1');
+            
+      res.send(`${rows[0].title} har lagts till från ${rows[0].release_year}`);
+
+*/
+    res.sendFile(path.join(__dirname, "..", "public", "overview.html"));
   };
 }
